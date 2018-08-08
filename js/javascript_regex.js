de0300a79b59a0777ca2c822a7fd0a0f0d5e3a1d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-08 12:48:10 -0500

    JavaScript: Refactoriza expresión regular que admite TSLD, cambia color del input al hacer match

--- js/javascript_regex.js
+++ js/javascript_regex.js
@@ -1,9 +1,20 @@
 function validate( str ) {
+    const $element = document .getElementById( 'in' );
     console .log( 'Valor: ' + str );
 
     // Valida nuestra expresión regular
-    if( str .match( /^[^@_]{3,10}@[\w\.]{3,12}\.\w{2,5}$/i ) ) {
+    if( str .match( /^[^@_]{3,10}@[\w\.]{3,12}\.\w{2,5}(\.\w{2,5})?$/i ) ) {
         document .getElementById( 'enviar' ) .disabled = false;
+        changeColor( $element, 'lightgreen' );
         console .log( 'Match!!' );
     }
+    else {
+        document .getElementById( 'enviar' ) .disabled = true;
+        changeColor( $element, 'red' );
+        console .log( 'No Match!!' );
+    }
+}
+
+function changeColor( $el, color ) {
+    $el .style .backgroundColor = color;
 }
