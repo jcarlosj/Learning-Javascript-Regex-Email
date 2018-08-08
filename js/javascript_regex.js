8dcc037c695f1c78197db3ebd84f823d511aa044
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-08 12:23:41 -0500

    JavaScript: Crea expresión regular que valida correo electrónico y activa botón

--- js/javascript_regex.js
+++ js/javascript_regex.js
@@ -1,3 +1,9 @@
 function validate( str ) {
     console .log( 'Valor: ' + str );
+
+    // Valida nuestra expresión regular
+    if( str .match( /^[^@_]{3,10}@[\w\.]{3,12}\.\w{2,5}$/i ) ) {
+        document .getElementById( 'enviar' ) .disabled = false;
+        console .log( 'Match!!' );
+    }
 }
